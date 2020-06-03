b9f8cdea83347d57de6e8d83bf96fe84ca973915
mchavez-pe <mauchp0311@gmail.com>
2020-06-03 01:33:06 -0500

    modificaciones realizadas al intranet mantenimiento

--- js/main.js
+++ js/main.js
@@ -11,7 +11,9 @@ listarOperaciones = null,
     flag = false,
     login = null,
     subirImagen = null,
-    getIdCuenta = null;
+    getIdCuenta = null,
+    logoff = null,
+    buscarEnTabla = null;
 $("document").ready(function () {
     var idCuenta;
 
@@ -22,6 +24,54 @@ $("document").ready(function () {
 
         return dia[0] + "/" + dia[1] + "/" + dia[2] + " " + data[1];
     }
+    function getCellValue(tr, idx) {
+        return tr.children[idx].innerText || tr.children[idx].textContent;
+    }
+    logoff = function () {
+        console.log("saliste");
+        // en observacion
+
+        var data = new FormData();
+        data.append("logoff", "1");
+        var obj = {
+            method: 'POST',
+            body: data
+        };
+        post(URL + "/Login", obj)
+            .then(function (response) {
+                console.log(response);
+                if (response.mensaje == "Session Cerrada") {
+                    console.log("saliste")
+                    window.location.href = window.location.origin + "/Login.html";
+                } else {
+                    console.log("sigues en la session ")
+
+                }
+            })
+    }
+
+    function comparer(idx, asc) {
+        return function (a, b) {
+            return function (v1, v2) {
+                return v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2);
+            }(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+        }
+    };
+
+    document.querySelectorAll("th").forEach(function (th) {
+        th.addEventListener("click", function () {
+            // tbody
+            const table = th.closest("table").children[1];
+            console.log(th.parentNode.children)
+            // get trs
+            Array.from(table.querySelectorAll('tr:nth-child(n)'))
+
+                .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+                .forEach(tr => table.appendChild(tr));
+
+
+        })
+    })
     if (window.location.pathname == "/Login.html") {
         login = function () {
             var user = document.getElementById("usuario").value;
@@ -44,7 +94,7 @@ $("document").ready(function () {
     }
     if (window.location.pathname == "/ListarOperaciones.html") {
         activarOperacion = function (event) {
-            // debugger;
+            document.getElementById("bg-spinner").style.visibility = "visible"
             var id = event.parentElement.parentElement.parentElement.getAttribute("id");
             var form = new FormData();
             form.append("Id", id);
@@ -58,12 +108,15 @@ $("document").ready(function () {
                     document.getElementById("modalMensaje").innerText = response.mensaje;
                     $('#alertaModificacion').modal('show');
                     // alert(response.mensaje)
+                    listarOperaciones();
+                    document.getElementById("bg-spinner").style.visibility = "hidden"
                 })
         }
         eliminarOperacion = function (event) {
             var id = JSON.parse(sessionStorage.getItem("ID"));
-            console.log(id.correo);  
-            
+            console.log(id.correo);
+
+            document.getElementById("bg-spinner").style.visibility = "visible"
             var form = new FormData();
             form.append("DeleteOperacion", id.id);
             form.append("Correo", id.correo);
@@ -75,8 +128,11 @@ $("document").ready(function () {
                 .then(function (response) {
                     console.log(response)
                     $("#alertaEliminacion").modal('hide');
-                    sessionStorage.removeItem("ID")
-                    
+                    sessionStorage.removeItem("ID");
+
+                    listarOperaciones();
+
+                    document.getElementById("bg-spinner").style.visibility = "hidden"
                 })
         }
         listarOperaciones = function () {
@@ -84,11 +140,11 @@ $("document").ready(function () {
                 method: 'POST'
                 // body:form
             }
+            $("#tbody").empty();
             post(URL + "/Admin", obj)
                 .then(function (response) {
-                    if (response.mensaje == "No hay elementos registrados") {
-                        console.log(response)
-                    } else {
+                    console.log(response)
+                    if (response.mensaje == "No hay elementos registrados") {} else {
 
                         var data = response;
                         var tbody = document.getElementById("tbody");
@@ -99,7 +155,6 @@ $("document").ready(function () {
                             tr.insertCell(0).innerText = element.usuario;
                             tr.insertCell(1).innerText = element.telefono;
                             tr.insertCell(2).innerText = element.correo;
-                            
                             tr.insertCell(3).innerText = formaterFecha(element.feOperacion);
                             tr.insertCell(4).innerText = element.nuCuenta;
                             tr.insertCell(5).innerText = element.numeroCuenta;
@@ -183,6 +238,8 @@ $("document").ready(function () {
             limpiarCampos();
         }
         eliminarCuenta = function (event) {
+            document.getElementById("bg-spinner").style.visibility = "visible"
+
             var id = JSON.parse(sessionStorage.getItem("ID"));
             var form = new FormData();
             $("#tbody").empty();
@@ -198,10 +255,13 @@ $("document").ready(function () {
                     $('#alertaEliminacion').modal('hide');
                     listarCuentas();
                     sessionStorage.removeItem("ID");
+                    document.getElementById("bg-spinner").style.visibility = "hidden"
+
                 })
         }
         editarCuenta = function (e) {
 
+            document.getElementById("bg-spinner").style.visibility = "visible"
 
             flag = true;
             var id = e.parentElement.parentElement.parentElement.getAttribute("id");
@@ -236,6 +296,8 @@ $("document").ready(function () {
                     // selectDepartamento(data.coDepartamento);
                     flag = false;
                     idCuenta = null;
+                    document.getElementById("bg-spinner").style.visibility = "hidden"
+
                 })
 
         }
@@ -249,10 +311,11 @@ $("document").ready(function () {
         subirImagen = function () {
             var img = document.getElementById("subidaimg");
             // console.log(img.files[0])
+            debugger;
             var form = new FormData();
             form.append("SubirImg", 1);
             form.append("Imagen", img.files[0]);
-            if (img.files.files >= 1) {
+            if (img.files.length >= 1) {
 
                 var obj = {
                     method: 'POST',
@@ -286,8 +349,6 @@ $("document").ready(function () {
                             tr.insertCell(1).innerText = element.precio;
                             tr.insertCell(2).innerHTML = `<div class='d-flex'>
                         <a title='Editar Operacion' onclick='editarPromocion(this);' class="btn btn-light"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                        <a  title='Eliminar Cuenta' onclick='eliminarPromocion(this);' class="btn btn-light"><i class="fa fa-trash-o"
-                        aria-hidden="true"></i></a>
                         </div>`;
                             tbody.appendChild(tr);
                         })
@@ -295,6 +356,7 @@ $("document").ready(function () {
                 })
 
         }
+
         listarPromociones();
         editarPromocion = function (e) {
             var id = e.parentElement.parentElement.parentElement.getAttribute("id");
@@ -353,13 +415,33 @@ $("document").ready(function () {
     getIdCuenta = function (e) {
         var obj = {};
         var id = e.parentElement.parentElement.parentElement.getAttribute("id");
-        if(document.title =="Lista de Operaciones"){
-            var correo= e.parentElement.parentElement.parentElement.children[2].innerText;
+        if (document.title == "Lista de Operaciones") {
+            var correo = e.parentElement.parentElement.parentElement.children[2].innerText;
             obj.correo = correo;
         }
         //  console.log(correo);
-        obj.id=id;
+        obj.id = id;
 
         sessionStorage.setItem("ID", JSON.stringify(obj));
+    }
+    buscarEnTabla = function () {
+        var entrada = document.getElementById("entrada").value;
+        var tbody = document.getElementById("dataTable").children[1];
+        var trs = tbody.getElementsByTagName("tr");
+        var cell;
+        for (let i = 0; i < trs.length; i++) {
+            trs[i].style.display = 'none';
+            var tds = trs[i].getElementsByTagName("td");
+            for (let j = 0; j < tds.length; j++) {
+                cell = tds[j];
+                if (cell.innerText.toUpperCase().indexOf(entrada.toUpperCase()) > -1) {
+                    trs[i].style.display = '';
+                    break;
+                }
+
+            }
+
+        }
+
     }
 })
